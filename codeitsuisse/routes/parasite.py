ec2c9f0beaf9373da064a1ca97df3f3175c8ce45
SherryWu178 <sherry.wu178@gmail.com>
2021-09-25 21:12:44 +0800

    Update parasite.py

--- codeitsuisse/routes/parasite.py
+++ codeitsuisse/routes/parasite.py
@@ -27,12 +27,12 @@ def evaluate_parasite():
         grid = room_data["grid"]
         interestedIndividuals = room_data["interestedIndividuals"]
         
-        a, b = part_1(grid, interestedIndividuals)
+        a, b, part4 = part_1(grid, interestedIndividuals)
         room_ans["room"] = room_index
         room_ans["p1"] = a
         room_ans["p2"] = b
         room_ans["p3"] = part_3(grid, interestedIndividuals)
-        room_ans["p4"] = 1
+        room_ans["p4"] = part4
         ans.append(room_ans) 
     
     logging.info("My result :{}".format(ans))
@@ -41,12 +41,6 @@ def evaluate_parasite():
 
 def check_infectable(grid, row, col):
     return grid[row][col] == HEALTHY
-
-def check_infectable_part4(grid, row, col):
-    return grid[row][col] == HEALTHY or grid[row][col] == VACCINATED or grid[row][col] == VACANT
-
-def need_energy(grid, row, col):
-    return grid[row][col] == VACCINATED or grid[row][col] == VACANT
 
 def part_1(grid, interestedIndividuals):
     infected_time = dict.fromkeys(interestedIndividuals, -1)
@@ -67,8 +61,6 @@ def part_1(grid, interestedIndividuals):
 
     while True:
         frontier = deque()
-        print("part 1 round ", round)
-        print("queue", infected_q)
         while infected_q:
             [row, col] = infected_q.popleft()
             
@@ -96,12 +88,90 @@ def part_1(grid, interestedIndividuals):
             break
 
     energy = 0
+    part4_ans = 0
+    
     for row in range(nRows):
         for col in range(nCols):
             if grid[row][col] == HEALTHY:
                 round = -1
+                part4_ans = part4(grid)
+                break
+
     
-    return infected_time, round
+    return infected_time, round, part4_ans
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+
+    def find(self, u):
+        if u != self.parent[u]:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def union(self, u, v):
+        pu, pv = self.find(u), self.find(v)
+        if pu == pv: return False
+        self.parent[pu] = pv
+        return True
+
+
+def part4(grid):
+    def manhattanDist(p1, p2):
+        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]) - 1 
+
+    grid = [x[:] for x in grid]
+    nRows = len(grid)
+    nCols = len(grid[0])
+    print(grid)
+    edges = []
+    n = 0
+    dic = {}
+    for row in range(nRows):
+        for col in range(nCols):
+            if grid[row][col] == VACANT or grid[row][col] == VACCINATED :
+                continue
+            if row*(nCols)+ col not in dic:
+                dic[row*(nCols)+ col] = n
+                n += 1
+
+            infected_a =  (grid[row][col] == INFECTED)
+            
+            for i in range(nRows):
+                for j in range(nCols):
+                    if (row == i and col == j) or grid[i][j] == VACANT or grid[i][j] == VACCINATED :
+                        continue
+                    else:
+                        if (i*(nCols)+j) not in dic:
+                            dic[i*(nCols)+j] = n
+                            n += 1
+                        infected_b =  (grid[i][j] == INFECTED)
+                        if infected_a and infected_b:
+                            if dic[i*(nCols)+j] > dic[row*(nCols)+ col]:
+                                edges.append([0, dic[row*(nCols)+ col], dic[i*(nCols)+j]])
+                        else:
+                            if dic[i*(nCols)+j] > dic[row*(nCols)+ col]:
+                                edges.append(
+                                    [manhattanDist([row, col], [i, j]), 
+                                    dic[row*(nCols)+ col], 
+                                    dic[i*(nCols)+j]])
+    
+
+    edges.sort()  # Sort increasing order by dist
+    print("edges: ", edges)
+    uf = UnionFind(n)
+    ans = 0
+    for d, u, v in edges:
+        if uf.union(u, v):
+            print(u,v)
+            print(d)
+            ans += d
+            n -= 1
+        if n == 1: 
+            break  # a bit optimize when we found enough n-1 edges!
+    print(ans)
+    return ans  
+
 
 def part_3(grid, interestedIndividuals):
     grid = [x[:] for x in grid]
@@ -118,8 +188,6 @@ def part_3(grid, interestedIndividuals):
 
     while True:
         frontier = deque()
-        print("part 3 round ", round)
-        print("queue", infected_q)
    
         while infected_q:
             [row, col] = infected_q.popleft()
